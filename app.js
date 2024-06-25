203675f70e5bd80010a754c8d4aa33b441b15a11
Antti33 <antti.laine@hotmail.fi>
2024-06-25 20:03:01 +0300

    Jotai säätöö

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+import * as songService from "./songService.js"
+
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World!"));
+app.get("/", async (c) => {
+    const data = {
+      songs: await songService.listSongs(),
+    };
+
+    return c.html(
+      eta.render("index.eta", data),
+    );
+  },
+);
+
+app.post("/songs", async (c) => {
+  const body = await c.req.parseBody();
+  await songService.addSong(body);
+  return c.redirect("/");
+});
 
 export default app;
